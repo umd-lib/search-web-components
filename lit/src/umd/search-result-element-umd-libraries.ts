47e09f16fc07bad0b903bbec74420d1e99f49340
Jeremy M Gottwig <jgottwig@umd.edu>
2025-08-29 12:27:19 -0400

    LIBWEB-6498. Altering fields config format.
    -Now objects that can contain configuration options.
    
    https://umd-dit.atlassian.net/browse/LIBWEB-6498

--- lit/src/umd/search-result-element-umd-libraries.ts
+++ lit/src/umd/search-result-element-umd-libraries.ts
@@ -1,6 +1,7 @@
 import { html, nothing } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 import { BaseSearchElement } from '../BaseSearchElement';
+import {unsafeHTML} from 'lit/directives/unsafe-html.js';
 
 /**
  * A default result render element that shows the raw JSON for each result in a collapsible details element.
@@ -29,13 +30,14 @@ export class SearchResultElementUMDLibraries extends BaseSearchElement {
     const id_field = this.settings['id'] as result_fields;
     const title_field = this.settings['title'] as result_fields;
     const thumbnail_field = this.settings['thumbnail'] as result_fields;
-    const list_fields: string = this.settings['fields'];
+    const fields = this.settings['fields_array'];
     const base_path: string = this.settings['base_path'];
 
     let id = this.data[id_field];
     let title = this.data[title_field];
     let thumbnail = this.data[thumbnail_field]
-    const fields = list_fields.split(',').map(field => field as result_fields);
+    let field_output = '' 
+    console.log(fields)
 
     if (Array.isArray(title)) {
       title = title.map((t: string) => t.startsWith('[@') ? t.split(']')[1] : t);
@@ -46,19 +48,32 @@ export class SearchResultElementUMDLibraries extends BaseSearchElement {
       thumbnail = thumbnail[0];
     }
 
+    for(var key in fields) {
+      if (fields.hasOwnProperty(key)) {
+        let displayed_field = fields[key].key
+        let show_label = fields[key].show_label
+        if (this.data[displayed_field]) {
+          if (show_label && show_label == "true") {
+            field_output += '<li><strong>' + key + ':</strong> ' + this.data[displayed_field] + '</li>'
+          } else {
+            field_output += '<li>' + this.data[displayed_field] + '</li>'
+          }
+        }
+      }
+    }
+
+    let revised_id = id.replace("solr_document/")
+
     return html`
       <h2>
-        <a href="${base_path + id}">
+        <a href="${base_path + revised_id}">
           ${title}
         </a>
       </h2>
 
       ${ thumbnail === 'static:unavailable' ? nothing : html`<img src="${thumbnail}" />` }
-      <ul>
-        ${fields.map(field =>
-          field === 'id' ? html`<li hidden> ${this.data[field]} </li>` : html`<li> ${this.data[field]} </li>`
-        )}
-      </ul>
+
+      ${ html`<ul> ${unsafeHTML(field_output)} </ul>` }
       <hr />
     `;
   }
