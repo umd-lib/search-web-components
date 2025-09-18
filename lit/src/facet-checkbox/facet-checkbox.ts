76b61d5b16ed23c9da1ced2a66cad8d5e3cb83b0
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2025-09-18 16:32:07 -0400

    LIBWEB-6489: Adding “Show More” and “Search Term” functionality
    Moved the rendering of a single facet to its own function, rather
    than as a anonymous function in the repeat directive
    
    Added logic for determing when to show the "Show More" button
    
    Added logic for determing if the facets should be limited at the initial
    render
    
    Added an input box and logic for filtering facets based on the search
    term
    
    https://umd-dit.atlassian.net/browse/LIBWEB-6489

--- lit/src/facet-checkbox/facet-checkbox.ts
+++ lit/src/facet-checkbox/facet-checkbox.ts
@@ -1,8 +1,8 @@
-import {html, TemplateResult} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import {repeat} from 'lit/directives/repeat.js';
+import { html, nothing, TemplateResult } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { repeat } from 'lit/directives/repeat.js';
 import BaseFacetElement from '../BaseFacetElement';
-import {ResultsType} from '../types';
+import { ResultsType, SearchFacetsType } from '../types';
 
 /**
  * A facet checkbox element.
@@ -12,8 +12,16 @@ export class FacetCheckbox extends BaseFacetElement {
   /**
    * Use radio inputs instead of checkboxes. Also ensures only one option can be chosen at a time.
    */
-  @property({type: Boolean})
+  @property({ type: Boolean })
   useRadios = false;
+
+  @state()
+  filterText: string = '';
+
+  @state()
+  showAllFacets: boolean = false;
+
+  readonly FACET_LIMIT = 10;
 
   override updated(changedProperties: Map<string, any>) {
     if (this.facetConfigLoaded || !this.context?.responseReady) {
@@ -56,50 +64,87 @@ export class FacetCheckbox extends BaseFacetElement {
     ) {
       opts = opts.slice(0, this.softLimit);
     }
+
+    if (this.filterText && this.filterText.trim() !== '') {
+      const filter = this.filterText.trim().toLowerCase();
+      opts = opts.filter(opt => opt.label.toLowerCase().includes(filter));
+    }
+
+    const showToggle = opts.length > this.FACET_LIMIT;
+    const limitFacets = opts.length > this.FACET_LIMIT && !this.showAllFacets;
+
+    const shown = limitFacets ? opts.slice(0, this.FACET_LIMIT) : opts;
+    const hidden = limitFacets ? opts.slice(this.FACET_LIMIT) : [];
+
     return html`
+      <input
+        type="text"
+        placeholder="Search Term"
+        .value=${this.filterText}
+        @input=${(e: Event) => {
+          const target = e.target as HTMLInputElement;
+          this.filterText = target.value;
+        }}
+      />
+
       <ul>
         ${repeat(
-          opts,
+          shown,
           (value) => value.key,
-          (value) => {
-            const id = this.genUniqId(
-              `${facet.key}-${this.safeIdentifier(value.key)}`
-            );
-
-            const classes = ['facet'];
-            this.useRadios ? classes.push('radio') : classes.push('checkbox');
-            value.active ? classes.push('active') : null;
-            value.in_active_trail ? classes.push('active-trail') : null;
-            if (value.children.length > 0) {
-              classes.push('has-children');
-              value.children_expanded
-                ? classes.push('children-expanded')
-                : classes.push('children-hidden');
-            }
-            return html`
-              <li class="${classes.join(' ')}">
-                <input
-                  type="${this.useRadios ? 'radio' : 'checkbox'}"
-                  id=${id}
-                  name="${value.key}"
-                  value="${value.key}"
-                  .checked=${value.active ?? false}
-                  ?checked=${value.active ?? false}
-                  @click=${() => this.applyFacet(this.urlAlias, value.key)}
-                />
-                <label for="${id}"
-                  >${value.label}${this.showCount
-                    ? ` (${value.count})`
-                    : null}</label
-                >
-                ${value.children.length > 0
-                  ? this._getOptions(value.children)
-                  : null}
-              </li>
-            `;
-          }
+          (value) => this._renderOption(value, facet, false)
         )}
+
+        ${repeat(
+          hidden,
+          (value) => value.key,
+          (value) => this._renderOption(value, facet, true)
+        )}
+
+        ${showToggle ?
+        html`<button @click=${() => { this.showAllFacets = !this.showAllFacets }}>
+            ${this.showAllFacets ? 'Show Less' : 'Show More'}
+          </button>`
+        : nothing}
       </ul>
+    `;
+  }
+
+  /**
+   * Renders a single facet
+   */
+  _renderOption(value: ResultsType, facet: SearchFacetsType, hide: boolean): TemplateResult {
+    const id = this.genUniqId(
+      `${facet.key}-${this.safeIdentifier(value.key)}`
+    );
+
+    const classes = ['facet'];
+    this.useRadios ? classes.push('radio') : classes.push('checkbox');
+    value.active ? classes.push('active') : null;
+    value.in_active_trail ? classes.push('active-trail') : null;
+    if (value.children.length > 0) {
+      classes.push('has-children');
+      value.children_expanded
+        ? classes.push('children-expanded')
+        : classes.push('children-hidden');
+    }
+    return html`
+      <li class="${classes.join(' ')}" ?hidden=${hide}>
+        <label for="${id}">
+          ${value.label}${this.showCount ? ` (${value.count})` : null}
+        </label>
+        <input
+          type="${this.useRadios ? 'radio' : 'checkbox'}"
+          id=${id}
+          name="${value.key}"
+          value="${value.key}"
+          .checked=${value.active ?? false}
+          ?checked=${value.active ?? false}
+          @click=${() => this.applyFacet(this.urlAlias, value.key)}
+        />
+        ${value.children.length > 0
+        ? this._getOptions(value.children)
+        : null}
+      </li>
     `;
   }
 
